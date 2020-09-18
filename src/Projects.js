3dc014e7a9b15f68a4ddd35be522259e12099923
Connor <cstreng95@gmail.com>
2020-09-18 20:32:28 +0200

    end of day

--- src/Projects.js
+++ src/Projects.js
@@ -17,7 +17,10 @@ const Projects = () => {
                         <div className="project-title eatstagram">
                             <h4>Eatstagram</h4>
                             <hr/>
-                            <p>A fun personal project where I keep and share recipes.</p>
+                            <p>
+                                A fun personal project where I keep and share recipes.
+                                Built with React and Google Firebase.
+                            </p>
                         </div>
 
                         {/* <div className="technology-container">
@@ -52,28 +55,37 @@ const Projects = () => {
                 </div>
 
                 <div className="project" id="project-2">
-                    <div className="project-title battleship">
-                        <h4>BATTLESHIP</h4>
-                        <p>This is my take on the classic two player board game.</p>
-                    </div>
+
                     <div className="image-container">
                         <img src={ BattleshipImage } alt=""/>
                     </div>
-                    <div className="button-container">
-                        <a href="https://ronnoc5991.github.io/battleship-react/" target="_blank">
-                            <div className="button">PROJECT</div>
-                        </a>
-                        <a href="https://github.com/ronnoc5991/battleship-react" target="_blank">
-                            <div className="button">CODE</div>
-                        </a>
+
+                    <div className="project-information">
+                        <div className="project-title battleship">
+                            <h4>BATTLESHIP</h4>
+                            <hr/>
+                            <p>This is my take on the classic two player board game.</p>
+                        </div>
+                        <div className="button-container">
+                            <a href="https://ronnoc5991.github.io/battleship-react/" target="_blank">
+                                <div className="button">PROJECT</div>
+                            </a>
+                            <a href="https://github.com/ronnoc5991/battleship-react" target="_blank">
+                                <div className="button">CODE</div>
+                            </a>
+                        </div>
                     </div>
-                    <div className="technology-container">
+                    
+                    {/* <div className="technology-container">
                         <h3>Built with:</h3>
                         <img src={ ReactImage } title="React" alt=""/>
                         <img src={ HTML } title="HTML" alt=""/>
                         <img src={ CSS } title="CSS" alt=""/>
                         <img src={ Jest } title="Jest" alt="" id="odd"/>
-                    </div> 
+                    </div>  */}
+
+
+
                     <div className="arrow-down">
                         <a href="#about">
                             <i className="fa fa-chevron-down fa-2x"></i>
