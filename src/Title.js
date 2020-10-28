a4826159d0c2d1b326eab568bb45a209bd602b32
Connor <cstreng95@gmail.com>
2020-10-28 18:02:04 +0100

    completes neon name for now

--- src/Title.js
+++ src/Title.js
@@ -1,52 +1,51 @@
 import React, { useRef, useEffect } from 'react'
 
 function Title() {
-
+    
     var container = useRef(null); 
     var text = useRef(null); 
+    var job = useRef(null); 
     
-
+    
     useEffect(() => {
         const tl = gsap.timeline(); //eslint-disable-line
-        tl.to(text, {color: "var(--light-on)", textShadow: "0 -40px 100px, 0 0 2px, 0 0 1em #516ace, 0 0 0.5em #516ace, 0 0 0.1em #516ace, 0 10px 3px #000", duration: .01, delay: 1.5});
-        // tl.to(container, {backgroundColor: "var(--background-on)", duration: .0});
-
+        tl.to(text, {color: "var(--light-on)", textShadow: "0 -5vh 10vh, 0 0 5px, 0 0 1em #516ace, 0 0 0.5em #516ace, 0 0 0.1em #516ace, 0 10px 3px #000", duration: .01, delay: 1.5});
+        
         tl.to(text, {color: "var(--light-off)", textShadow: "none", duration: .01, delay: .2});
-        // tl.to(container, {backgroundColor: "var(--background-off)", duration: .0});
-
-        tl.to(text, {color: "var(--light-on)", textShadow: "0 -40px 100px, 0 0 2px, 0 0 1em #516ace, 0 0 0.5em #516ace, 0 0 0.1em #516ace, 0 10px 3px #000", duration: .01, delay: .2});
-        // tl.to(container, {backgroundColor: "var(--background-on)", duration: .0});
-
+        
+        tl.to(text, {color: "var(--light-on)", textShadow: "0 -5vh 10vh, 0 0 5px, 0 0 1em #516ace, 0 0 0.5em #516ace, 0 0 0.1em #516ace, 0 10px 3px #000", duration: .01, delay: .2});
+        
         tl.to(text, {color: "var(--light-off)", textShadow: "none", duration: .01, delay: .2});
-        // tl.to(container, {backgroundColor: "var(--background-off)", duration: .0});
+        
+        tl.to(text, {color: "var(--light-on)", textShadow: "0 -5vh 10vh, 0 0 5px, 0 0 1em #516ace, 0 0 0.5em #516ace, 0 0 0.1em #516ace, 0 10px 3px #000", duration: .01, delay: .2});
+        
+        tl.to('.border', {border: ".8vmin solid var(--light-on)", duration: .1, delay: 1})
 
-        tl.to(text, {color: "var(--light-on)", textShadow: "0 -40px 100px, 0 0 2px, 0 0 1em #516ace, 0 0 0.5em #516ace, 0 0 0.1em #516ace, 0 10px 3px #000", duration: .01, delay: .2});
-        // tl.to(container, {backgroundColor: "var(--background-on)", duration: .0});
+        tl.to(job, {color: "var(--light-on)", textShadow: "0 -1vh 5vh, 0 0 3px, 0 0 1em #516ace, 0 0 0.5em #516ace, 0 0 0.1em #516ace, 0 10px 3px #000", duration: .5})
 
     }, [])
 
     return (
-        <div className="Title view" ref={ el => { container = el } }>
+        <div className="Title" ref={ el => { container = el } }>
 
+            <div className="background"></div>
+
+            <div className="wrapper">
                 <div className="text" ref={ el => { text = el } }>
                     <span>C</span>onnor S<span>treng</span>
                 </div>
-
-            {/* <div class="name-container">
-                <div class="name" ref={ el => { name = el } }>
-                    Hi, I'm Connor
+                <div className="job" ref={ el => { job = el } }>
+                    Front End Developer
                 </div>
+                <div className="top-border border"></div>
+                <div className="left-border border"></div>
+                <div className="right-border border"></div>
             </div>
 
-            <div className="job" ref={ el => { description = el } }>
-                    I'm a <strong>Front End Developer</strong> based in <strong>Zeist, NL</strong>
-            </div>
+            {/* <div className="ground">
 
-            <div className="arrow-down" ref={ el => { arrowDown = el } }>
-                <a href="#project-1">
-                    <i className="fa fa-chevron-down fa-2x"></i>
-                </a>
             </div> */}
+
         </div>
     )
 }
